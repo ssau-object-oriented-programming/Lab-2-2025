c41c47d07fae0b09d7c33cc00ad0e049468f59a7
romchik302 <115893592+romchik302@users.noreply.github.com>
2025-10-15 01:47:22 +0400

    ver1

---
+++ functions/FunctionPoint.java
@@ -0,0 +1,45 @@
+package functions;
+
+public class FunctionPoint
+{
+    private double x;
+    private double y;
+
+    public FunctionPoint()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public FunctionPoint(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public FunctionPoint(FunctionPoint point)
+    {
+        this.x = point.getX();
+        this.y = point.getY();
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+
+    public void setY(double y)
+    {
+        this.y = y;
+    }
+}
---
+++ functions/TabulatedFunction.java
@@ -0,0 +1,152 @@
+package functions;
+
+class TabulatedFunction
+{
+    private FunctionPoint[] points;
+
+    public TabulatedFunction(double leftX, double rightX, int pointsCount)
+    {
+        if (pointsCount < 2)
+        {
+            throw new IllegalArgumentException("pointsCount must be at least 2");
+        }
+
+        if(leftX >= rightX)
+        {
+            throw new IllegalArgumentException("leftX must be less than rightX");
+        }
+
+        this.points = new FunctionPoint[pointsCount];
+        double step = (rightX - leftX) / (pointsCount - 1);
+
+        for(int i = 0; i < pointsCount; i++)
+        {
+            this.points[i] = new FunctionPoint(leftX + step * i, 0);
+        }
+    }
+
+    public TabulatedFunction(double leftX, double rightX, double[] values)
+    {
+        if (values.length < 2)
+        {
+            throw new IllegalArgumentException("pointsCount must be at least 2");
+        }
+
+        if(leftX >= rightX)
+        {
+            throw new IllegalArgumentException("leftX must be less than rightX");
+        }
+
+        this.points = new FunctionPoint[values.length];
+        double step = (rightX - leftX) / (values.length - 1);
+
+        for(int i = 0; i < values.length; i++)
+        {
+            this.points[i] = new FunctionPoint(leftX + step * i, values[i]);
+        }
+    }
+
+    public double getLeftDomainBorder()
+    {
+        return this.points[0].getX();
+    }
+
+    public double getRightDomainBorder()
+    {
+        return this.points[this.points.length - 1].getX();
+    }
+
+    public double getFunctionValue(double x)
+    {
+        if(x < this.points[0].getX() || x > this.points[points.length - 1].getX())
+        {
+            return Double.NaN;
+        }
+
+        for(int i = 0; i < this.points.length - 1; i++)
+        {
+            double x1 = this.points[i].getX();
+            double x2 = this.points[i + 1].getX();
+
+            if(x == x1)
+            {
+                return this.points[i].getY();
+            }
+
+            if(x == x2)
+            {
+                return  this.points[i + 1].getY();
+            }
+
+            if(x > x1 && x < x2)
+            {
+                double y1 = this.points[i].getY();
+                double y2 = this.points[i + 1].getY();
+
+                return y1 + (y2 - y1) * (x - x1) / (x2 - x1);
+            }
+        }
+
+        return Double.NaN;
+    }
+
+    public int pointCount()
+    {
+        return this.points.length;
+    }
+
+    public FunctionPoint getPoint(int index)
+    {
+        if(index < 0)
+        {
+            throw new IndexOutOfBoundsException("Index cannot be negative");
+        }
+
+        if(index >= this.points.length)
+        {
+            throw new IndexOutOfBoundsException("Index exceed maximum index");
+        }
+
+        return new FunctionPoint(this.points[index]);
+    }
+
+    public void setPoint(int index, FunctionPoint point)
+    {
+        if(index < 0)
+        {
+            throw new IndexOutOfBoundsException("Index cannot be negative");
+        }
+
+        if(index >= this.points.length)
+        {
+            throw new IndexOutOfBoundsException("Index exceed maximum index");
+        }
+
+        if (index == 0)
+        {
+            if (points.length > 1 && point.getX() >= points[1].getX())
+            {
+                throw new IllegalArgumentException("First point x must be less than next point x");
+            }
+        }
+        else if (index == points.length - 1)
+        {
+            if (point.getX() <= points[index - 1].getX())
+            {
+                throw new IllegalArgumentException("Last point x must be greater than previous point x");
+            }
+        }
+        else
+        {
+            if (point.getX() <= points[index - 1].getX() || point.getX() >= points[index + 1].getX())
+            {
+                throw new IllegalArgumentException("Point x must be between neighbors");
+            }
+        }
+
+        points[index] = new FunctionPoint(point);
+    }
+
+
+
+}
