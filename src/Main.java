312846fd3f9f3ddef5476c92f3e8be119981cac2
lizacode39 <elizavetaivanova200@yandex.ru>
2025-10-22 11:06:06 +0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,33 @@ public class Main {
 
         System.out.println("\nТочки:");
         tab.printTabFun();
+        System.out.println("\nПРОВЕРКА СЕТТЕРОВ");
+
+        double testVals[] = {0, 4, 8};
+        TabulatedFunction testFunc = new TabulatedFunction(0, 4, testVals);
+
+        System.out.println("Исходные точки:");
+        testFunc.printTabFun();
+
+// 1. Корректное изменение Y (всегда разрешено)
+        testFunc.setPointY(1, 10);
+        System.out.println("\nПосле setPointY(1, 10):");
+        testFunc.printTabFun(); // y[1] должно стать 10
+
+// 2. Корректное изменение X (вписывается в интервал)
+        testFunc.setPointX(1, 1.5); // между 0 и 4 → допустимо
+        System.out.println("\nПосле setPointX(1, 1.5):");
+        testFunc.printTabFun(); // x[1] = 1.5
+
+// 3. Некорректное изменение X (нарушает порядок)
+        testFunc.setPointX(1, 5); // 5 > 4 (следующая точка) → должно игнорироваться
+        System.out.println("\nПосле попытки setPointX(1, 5) (некорректно):");
+        testFunc.printTabFun(); // x[1] останется 1.5
+
+// 4. Попытка заменить первую точку на x=3 (должно игнорироваться)
+        testFunc.setPoint(0, new FunctionPoint(3, -1));
+        System.out.println("\nПосле попытки setPoint(0, (3,-1)) (некорректно):");
+        testFunc.printTabFun(); // первая точка останется (0,0)
+
     }
 }
