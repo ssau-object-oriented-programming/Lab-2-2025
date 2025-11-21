0a4d078e507f42a9e1abfc197ca05e4d2c95858a
Alexis-xmyriy <trifimovav05@gmail.com>
2025-11-21 20:29:39 +0300

    Lab2

---
+++ functions/FunctionPoint.java
@@ -0,0 +1,43 @@
+package functions;
+
+public class FunctionPoint {
+    private double x;
+    private double y;
+
+    // Конструктор с заданными координатами
+    public FunctionPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Конструктор копирования
+    public FunctionPoint(FunctionPoint point) {
+        this.x = point.x;
+        this.y = point.y;
+    }
+
+    // Конструктор по умолчанию (0, 0)
+    public FunctionPoint() {
+        this(0.0, 0.0);
+    }
+
+    // Геттер для координаты x
+    public double getX() {
+        return x;
+    }
+
+    // Геттер для координаты y
+    public double getY() {
+        return y;
+    }
+
+    // Сеттер для координаты x
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    // Сеттер для координаты y
+    public void setY(double y) {
+        this.y = y;
+    }
+}
